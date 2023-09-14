8c2dab4358b5b00c8bbfd2da339ab3d43ce8fc9c
yarrrr01 <144357273+yarrrr01@users.noreply.github.com>
2023-09-14 22:46:03 +0300

    Add files via upload

--- lecture-2/main.cpp
+++ lecture-2/main.cpp
@@ -8,27 +8,30 @@ public:
 	~FooString() {
 	};
 	void show() {
-		std::cout << buf << '\n';
+		 std::cout << buf << '\n';
 	};
-
+	
 	int lenght() {
-		int len = 0;
-		while (buf[len] != '\0') {
-			len += 1;
-		}
-		return len;
+	    int len = 0;
+	    while (buf[len] != '\0'){
+	        len += 1;
+	    }
+	    return len;
 	};
-	char* getStr() { // Вспомогательный метод
-		return buf;
+	char* getStr(){ //Additional method
+	    return buf;
 	}
 };
 
 int main() {
-	FooString str[] = { "Hello", "", "123456789The lenght of this string is 40" };
-	std::cout << "\"FooString\" - lenght of this FooString\n";
-	for (int i = 0; i < 3; i++) {
-		std::cout << '\"' << str[i].getStr() << "\" - " << str[i].lenght() << '\n';
-	}
+    FooString str[] = {"Hello", "", "123456789The lenght of this string is 40"};
+    std::cout << "\"FooString\" - lenght of this FooString\n";
+    for (int i = 0; i < 3; i++){
+        std::cout << '\"' << str[i].getStr() << "\" - " << str[i].lenght() << '\n';
+    }
+    
+    int a;
+    std::cin >> a;
 
 	return 0;
 }

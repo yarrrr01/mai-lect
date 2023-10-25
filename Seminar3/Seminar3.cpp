8464c953b60c21e1b211089ca84db1f0ff5e0291
yarrrr01 <144357273+yarrrr01@users.noreply.github.com>
2023-10-25 17:32:27 +0300

    Update Seminar3.cpp

--- Seminar3/Seminar3.cpp
+++ Seminar3/Seminar3.cpp
@@ -1,12 +1,14 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 #include <map>
 #include <random>
+#include <ctime>
 
-using std::string;
-using std::cout;
+
+using namespace std;
 
 uint32_t hashCode(string str) {
+
     int group, numOfSym;
     uint32_t hashCode;
     hashCode = 0;
@@ -62,35 +64,53 @@ string stringGen(int len) {
 }
 
 
-
-std::map<string, uint32_t> mapS;
-std::map<uint32_t, string> mapReversed;
 int main() {
-    std::srand(999); // 999, 899, 500, 12345, 0, 100000, 13, 2345, 34633
-    string s;
+    std::srand(34633); // 999, 899, 500, 12345, 0, 100000, 13, 2345, 34633  - это надо, чтобы генерировать при запуске генерировались одни и те же случайные числа
+    string str;
+    const int TOTAL_NUMBER_OF_STRINGS = 13600;
+    string strArr[TOTAL_NUMBER_OF_STRINGS];
+    int hashArr[TOTAL_NUMBER_OF_STRINGS];
+    int timeToCompareStrings(0), timeToCompareHashCodes(0), numOfSimilarStr(0), numOfSimilarHashCodes(0);
     
     for (int z = 3; z <= 7; z++) {
-        for (int i = 0; i < 10000; i++) {
-            s = stringGen(z);
-            mapS[s] = hashCode(s);
+        for (int i = 0; i < 10000; i++) { // генерация 10000 строк
+            str = stringGen(z);
+            strArr[i] = str;
+            hashArr[i] = hashCode(str);
         }
 
-        for (int i = 0; i < 100; i++) {
-            s = stringGen(z);
-            mapS[s] = hashCode(s);
+        for (int i = 0; i < 100; i++) { // генерация 100 наборов строк с 36 строками
+            str = stringGen(z);
             int idS = rand() % z;
             for (int j = 0; j < 36; j++) {
-                s[idS] = defineSym(j);
-                mapS[s] = hashCode(s);
+                str[idS] = defineSym(j);
+                strArr[10000 + 36*i + j] = str;
+                hashArr[10000 + 36*i + j] = hashCode(str);
             }
         }
-        std::map <string, uint32_t> ::iterator it = mapS.begin();
-        for (int i = 0; it != mapS.end(); it++, i++) {
-            mapReversed[it->second] = it->first;
+
+        int startTime = clock();
+        for (int i = 0; i < TOTAL_NUMBER_OF_STRINGS; i++) {
+            for (int j = i+1; j < TOTAL_NUMBER_OF_STRINGS; j++) {
+                if (strArr[i] == strArr[j]) numOfSimilarStr++;
+            }
         }
-        cout << "Number of collision for strings of length " << z << ":  " << mapS.size() - mapReversed.size() << '\n'; // кол-во коллизий
-        //cout << mapS.size() - mapReversed.size() << '\n';
-        mapS.clear();
-        mapReversed.clear();
+        timeToCompareStrings = clock() - startTime;
+        // ====
+        startTime = clock();
+        for (int i = 0; i < TOTAL_NUMBER_OF_STRINGS; i++) {
+            for (int j = i + 1; j < TOTAL_NUMBER_OF_STRINGS; j++) {
+                if (hashArr[i] == hashArr[j]) numOfSimilarHashCodes++;
+            }
+        }
+        timeToCompareHashCodes = clock() - startTime;
+
+        cout << "Number of collision for strings of length " << z << ":  " << numOfSimilarHashCodes - numOfSimilarStr << '\n'
+            << "Time to comparing strings: " << timeToCompareStrings << '\n'
+            << "Time to comparing hash codes: " << timeToCompareHashCodes << '\n' // кол-во коллизий / время сравнения строк / время сравнения чисел
+            << "=============================================\n";
+
+        numOfSimilarHashCodes = 0;
+        numOfSimilarStr = 0;
     }
 }
